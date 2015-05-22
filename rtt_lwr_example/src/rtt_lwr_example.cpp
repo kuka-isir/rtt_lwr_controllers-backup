1488d5ad038d9488d247b5da83f7480fa83f9fba
Antoine Hoarau <hoarau.robotics@gmail.com>
2015-05-22 11:57:58 +0200

    put to joint position control

--- rtt_lwr_example/src/rtt_lwr_example.cpp
+++ rtt_lwr_example/src/rtt_lwr_example.cpp
@@ -6,19 +6,19 @@
 
 lwr::RTTLWRExample::RTTLWRExample(const std::string& name): RTTLWRAbstract(name)
 {
-    RTT::log(RTT::Info) << "Example Created !" << RTT::endlog();
+    RTT::log(RTT::Info) << "Example created" << RTT::endlog();
 }
-lwr::RTTLWRExample::~RTTLWRExample()
-{
 
+bool lwr::RTTLWRExample::configureHook()
+{
+    bool configure = lwr::RTTLWRAbstract::configureHook();
+
+    this->setJointPositionControlMode();
+
+    return configure;
 }
 
 void lwr::RTTLWRExample::updateHook()
 {
-
+    //RTT::log(RTT::Info) << "updateHook() loop" << RTT::endlog();
 }
-bool lwr::RTTLWRExample::configureHook()
-{
-return lwr::RTTLWRAbstract::configureHook();
-}
-
