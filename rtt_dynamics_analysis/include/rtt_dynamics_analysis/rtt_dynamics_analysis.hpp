bbfcb330864a7b9f0f033b304a0ed22936bce70b
Antoine Hoarau <hoarau.robotics@gmail.com>
2015-10-18 17:45:48 +0200

    add boost deps (was removed from last commit)

--- rtt_dynamics_analysis/include/rtt_dynamics_analysis/rtt_dynamics_analysis.hpp
+++ rtt_dynamics_analysis/include/rtt_dynamics_analysis/rtt_dynamics_analysis.hpp
@@ -31,6 +31,8 @@
 #include <boost/circular_buffer.hpp>
 #include <boost/foreach.hpp>
 #include <boost/assign.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #ifdef CONMAN
 #include <conman/conman.h>
